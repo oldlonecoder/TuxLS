6cb5505c9de502dbb48fe0edbb5a456c94913bf9
oldlonecoder <lussier.serge@gmail.com>
2022-10-16 22:51:53 -0400

    Une premiere forme d'erreur provoquee ok;

--- Tests/Test.cc
+++ Tests/Test.cc
@@ -23,7 +23,7 @@ using Tux::Rem;
 Tux::Expect<> TestText()
 {
     using Tux::Text;
-    Text Txt = " Allo <Icon: ArrowRight; Color: Yellow, BlueViolet ;> &agrave; <Color : Reset> vous tous! ";
+    Text Txt = " Allo <Icon: ArrowRight; Color: Yellow, BlueViolet &agrave; <Color : Reset> vous tous! ";
     Txt.Compile();
     std::string Str;
     Txt >> Str;
